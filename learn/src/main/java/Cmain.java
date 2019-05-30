7c049890f0fd2fce8e823e0d655964fcdde79ed9
hgh <huangganghui@taqu.cn>
2019-05-30 18:24:03 +0800

    初始化

---
+++ learn/src/main/java/Cmain.java
@@ -0,0 +1,11 @@
+/**
+ * @Description TODO
+ * @Date 2019-05-30 18:23
+ * @Created by hgh
+ */
+public class Cmain {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
