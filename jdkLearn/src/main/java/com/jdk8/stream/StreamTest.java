c19323867b61ffec36747299083debd26888725c
hgh <huangganghui@taqu.cn>
2019-06-13 20:54:26 +0800

    代码上传

--- jdkLearn/src/main/java/com/jdk8/stream/StreamTest.java
+++ jdkLearn/src/main/java/com/jdk8/stream/StreamTest.java
@@ -1,10 +1,13 @@
 package com.jdk8.stream;
 
-import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author hgh
@@ -14,10 +17,76 @@ import java.util.stream.Collectors;
 public class StreamTest {
 
     @Test
-    public void testParrlelStream() {
-        List<String> strings = Lists.newArrayList("ca", "12dsdsfaa", "dcasaa", "sdf");
-        List<String> result = strings.stream().filter(e -> e.endsWith("a")).sorted(String::compareTo).collect(Collectors.toList());
-        System.out.println(result);
+    public void testsStream() {
+        // 起始时间
+        LocalTime start = LocalTime.now();
+
+        List<Integer> list = new ArrayList<>();
+        // 将10000-1存入list中
+        for (int i = 100000; i >= 1; i--) {
+            list.add(i);
+        }
+
+        list.stream()// 获取串行流
+                .sorted()// 按自然排序，即按数字从小到大排序
+                .count();// count()是终止操作，有终止操作才会执行中间操作sorted()
+
+        // 终止时间
+        LocalTime end = LocalTime.now();
+
+        // 时间间隔
+        Duration duration = Duration.between(start, end);
+        // 输出时间间隔毫秒值
+        System.out.println(duration.toMillis());
+    }
+
+    @Test
+    public void testParallelStream() {
+        // 起始时间
+        LocalTime start = LocalTime.now();
+
+        List<Integer> list = new ArrayList<>();
+        // 将10000-1存入list中
+        for (int i = 100000; i >= 1; i--) {
+            list.add(i);
+        }
+
+        list.parallelStream()// 获取并行流
+                .sorted()// 按自然排序，即按数字从小到大排序
+                .count();// count()是终止操作，有终止操作才会执行中间操作sorted()
+
+        // 终止时间
+        LocalTime end = LocalTime.now();
+
+        // 时间间隔
+        Duration duration = Duration.between(start, end);
+        // 输出时间间隔毫秒值
+        System.out.println(duration.toMillis());
+    }
+
+    /**
+     * 常见流的构造方法
+     */
+    @Test
+    public void testStream() {
+        // 1. Individual values
+        Stream stream = Stream.of("a", "b", "c");
+        // 2. Arrays
+        String[] strArray = new String[]{"a", "b", "c"};
+        stream = Stream.of(strArray);
+        stream = Arrays.stream(strArray);
+
+    }
+
+    public void toUppest() {
+        Stream<List<Integer>> inputStream = Stream.of(
+                Arrays.asList(1),
+                Arrays.asList(2, 3),
+                Arrays.asList(4, 5, 6)
+        );
+        Stream<Integer> outputStream = inputStream.
+                flatMap((childList) -> childList.stream());
+
     }
 
 
